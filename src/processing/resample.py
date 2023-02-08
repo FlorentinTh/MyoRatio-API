c00c00997571c537499e18bcfe55161205aad858
FlorentinTh <florentin.thullier1@uqac.ca>
2023-02-08 08:22:21 -0500

    style(resample): code lint

--- src/processing/resample.py
+++ src/processing/resample.py
@@ -13,5 +13,5 @@ class Resample:
         if self._fixed_value is None:
             frequency_fraction = Fraction(self._high_frequency / self._low_frequency).limit_denominator(1000)
             return signal.resample_poly(self._data, frequency_fraction.numerator, frequency_fraction.denominator)
-        else :
+        else:
             return signal.resample_poly(self._data, self._fixed_value, len(self._data))
