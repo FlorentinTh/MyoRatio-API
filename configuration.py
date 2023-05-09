b37858cee8ffc240eef1c16de5ea340f51855b61
FlorentinTh <florentin.thullier1@uqac.ca>
2023-05-09 09:39:37 -0400

    refactor(configuration): remove test api key

--- configuration.py
+++ configuration.py
@@ -4,4 +4,4 @@ from enum import Enum
 class Configuration(Enum):
     HOST = "127.0.0.1"
     # deepcode ignore HardcodedNonCryptoSecret: both GUI and API are running locally anyway
-    API_KEY = "c5cd556a0983"
+    API_KEY = "<your_api_key>"
