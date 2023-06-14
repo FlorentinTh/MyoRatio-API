acf57cfd0373b93333e0b0e6a68264fb3f0f0acd
FlorentinTh <florentin.thullier1@uqac.ca>
2023-06-14 09:12:58 -0400

    feat(ratio): add enum for all muscles that may be part of the ratio computation

--- myoratio/results/ratio.py
+++ myoratio/results/ratio.py
@@ -1,12 +1,29 @@
-from myoratio.task import Analysis
+from enum import Enum
+
+from myoratio.task import Analysis, Stage
+
+
+class Muscles(Enum):
+    RECTUS_FEMORIS = "RECTUS FEMORIS"
+    RECTUS_ABDOMINIS = "RECTUS ABDOMINIS"
+    TENSOR_FASCIAE_LATAE = "TENSOR FASCIAE LATAE"
+    GLUTEUS_MAXIMUS = "GLUTEUS MAXIMUS"
+    BICEPS_FEMORIS = "BICEPS FEMORIS"
+    ADDUCTOR_LONGUS = "ADDUCTOR_LONGUS"
+    TIBIALIS_ANTERIOR = "TIBIALIS ANTERIOR"
 
 
 class Ratio:
-    def __init__(self, analysis: str) -> None:
+    def __init__(self, analysis: str, stage: str) -> None:
         if analysis in [analysis.value for analysis in Analysis]:
             self._analysis = analysis
         else:
             raise ValueError(f"Expected a value from Analysis, but got {analysis}")
+
+        if stage in [stage.value for stage in Stage]:
+            self._stage = stage
+        else:
+            raise ValueError(f"Expected a value from Stage, but got {stage}")
 
     def get_muscles(self) -> tuple[str | None, str | None]:
         antagonist = None
@@ -17,8 +34,13 @@ class Ratio:
             or self._analysis == Analysis.FLEXION.value
             or self._analysis == Analysis.SIT_STAND.value
         ):
-            antagonist = "BICEPS FEMORIS"
-            agonist = "RECTUS FEMORIS"
+            if self._stage == Stage.CONCENTRIC.value:
+                antagonist = Muscles.RECTUS_FEMORIS.value
+                agonist = Muscles.BICEPS_FEMORIS.value
+            else:
+                antagonist = Muscles.BICEPS_FEMORIS.value
+                agonist = Muscles.RECTUS_FEMORIS.value
+
         else:
             raise ValueError(f"Muscles for analysis {self._analysis} are not defined")
 
