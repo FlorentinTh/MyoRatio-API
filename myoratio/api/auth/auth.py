948be15c1a2d2e31ae905ec6244429c6fc462796
FlorentinTh <florentin.thullier1@uqac.ca>
2023-07-13 11:34:03 -0400

    feat(auth): override handler for 401 errors

--- myoratio/api/auth/auth.py
+++ myoratio/api/auth/auth.py
@@ -1,7 +1,7 @@
 import bcrypt
 from flask_httpauth import HTTPTokenAuth
 
-from myoratio.api import Configuration
+from myoratio.api import API, Configuration
 from myoratio.api.utils import APIKey
 
 configuration = Configuration.load()
@@ -13,3 +13,8 @@ api_key_hashed = APIKey.hash_key(str(configuration["API_KEY"]))
 @auth.verify_token
 def verify_api_key(key: str) -> bool:
     return bcrypt.checkpw(key.encode("utf-8"), api_key_hashed)
+
+
+@auth.error_handler
+def auth_error(status):
+    return API.error_response(status, "Access Denied", "Please check the API_KEY")
